d21d2cf2f00f9b04092a2b3d4e291e5f341dae43
Dwayne Harris <dwayne@dwayne.xyz>
2022-01-10 11:31:19 -0500

    Update static LD flags to run on macOS

--- cmd/ffi-debug/main_static.go
+++ cmd/ffi-debug/main_static.go
@@ -4,7 +4,7 @@ package main
 // The -ldl is necessary to fix the linker errors about `dlsym` that would otherwise appear.
 
 /*
-#cgo LDFLAGS: ./lib/libhostbridge.a -ldl
+#cgo LDFLAGS: ./lib/libhostbridge.a -ldl -framework Carbon -framework Cocoa -framework CoreFoundation -framework CoreVideo -framework IOKit -framework WebKit
 #include "../../lib/hostbridge.h"
 */
 import "C"
