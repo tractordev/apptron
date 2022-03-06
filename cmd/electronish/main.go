3946bed6c5569d6ef3bb7a2dbcc2e388feb98a02
Jeff Lindsay <progrium@gmail.com>
2022-03-05 20:04:24 -0600

    outline electronish

---
+++ cmd/electronish/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// run hostbridge
+	// serve local dir over http
+	// accept qtalk ws connections, pass channels to hostbridge
+	// read window options from index.html, show window
+}
