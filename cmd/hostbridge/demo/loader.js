666e188c98c281853b19bb2ab4ab58220fb802e7
Jeff Lindsay <progrium@gmail.com>
2022-03-19 14:47:31 -0500

    demo: cleanup

--- cmd/hostbridge/demo/loader.js
+++ cmd/hostbridge/demo/loader.js
@@ -2,6 +2,7 @@ window["$host"] = {
   ready: new Promise((resolve) => {
     import("/-/client.js").then(async (mod) => {
       window["$host"] = await mod.connect(`ws://${window.location.host}/`)
+      window["$host"].ready = Promise.resolve()
       resolve()
     })
   })
