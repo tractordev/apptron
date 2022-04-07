c866176af67ef09d27e32c9193b2203e7887e4d0
Jeff Lindsay <progrium@gmail.com>
2022-04-07 17:59:13 -0500

    top level package that imports client (also experimental type aliases)

---
+++ apptron.go
@@ -0,0 +1,20 @@
+package apptron
+
+import "tractor.dev/apptron/client"
+
+// these aliases are experimental
+// and may disappear
+
+type Client = client.Client
+type Window = client.Window
+type Menu = client.Menu
+type MenuItem = client.MenuItem
+type FileDialog = client.FileDialog
+type MessageDialog = client.MessageDialog
+type Notification = client.Notification
+type Display = client.Display
+type AppOptions = client.AppOptions
+type WindowOptions = client.WindowOptions
+type Size = client.Size
+type Position = client.Position
+type Handle = client.Handle
