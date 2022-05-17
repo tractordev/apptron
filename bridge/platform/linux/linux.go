c86b4e137a82d6d5edff7e47e5fcb55fdd2d8623
Nick Aversano <nick23hi@gmail.com>
2022-05-17 17:21:54 -0400

    remove linux.c file

--- bridge/platform/linux/linux.go
+++ bridge/platform/linux/linux.go
@@ -13,6 +13,7 @@ import (
 */
 import "C"
 
+
 type Menu_Callback func(menuId int)
 
 var globalMenuCallback Menu_Callback
@@ -22,50 +23,105 @@ var globalMenuCallback Menu_Callback
 //
 
 func OS_Init() {
-	C.tray_init()
+	C.gtk_init_check(nil, nil)
 }
 
 func PollEvents() {
-	C.tray_poll_events()
+	C.gtk_main_iteration_do(0)
 }
 
 func NewIndicator(id string, pngIconPath string, menu MenuHandle) IndicatorHandle {
 	cid := C.CString(id)
 	defer C.free(unsafe.Pointer(cid))
 
-	cIconPath := C.CString(pngIconPath)
-	defer C.free(unsafe.Pointer(cIconPath))
+	result := C.app_indicator_new(cid, C.CString(""), C.APP_INDICATOR_CATEGORY_APPLICATION_STATUS)
+	C.app_indicator_set_status(result, C.APP_INDICATOR_STATUS_ACTIVE)
 
-	result := C.tray_indicator_new(cid, cIconPath, fromMenuHandle(menu))
+	//app_indicator_set_title(global_app_indicator, title);
+	//app_indicator_set_label(global_app_indicator, title, "");
+
+	if len(pngIconPath) > 0 {
+		cIconPath := C.CString(pngIconPath)
+		defer C.free(unsafe.Pointer(cIconPath))
+
+		C.app_indicator_set_icon_full(result, cIconPath, C.CString(""))
+	}
+
+	if menu != 0 {
+		C.app_indicator_set_menu(result, (*C.struct__GtkMenu)(unsafe.Pointer(fromMenuHandle(menu))))
+	}
 
 	return toIndicatorHandle(result)
 }
 
 func MenuNew() MenuHandle {
-	result := C.menu_new()
+	result := C.gtk_menu_new()
 	return toMenuHandle(result)
 }
 
 func MenuAppendMenuItem(menu MenuHandle, item MenuItemHandle) {
-	C.menu_append_menu_item(fromMenuHandle(menu), fromMenuItemHandle(item))
+	menuShell := (*C.struct__GtkMenuShell)(unsafe.Pointer(fromMenuHandle(menu)))
+	C.gtk_menu_shell_append(menuShell, fromMenuItemHandle(item))
 }
 
 func MenuItemNew(id int, title string, disabled bool, checked bool, separator bool) MenuItemHandle {
-	ctitle := C.CString(title)
-	defer C.free(unsafe.Pointer(ctitle))
+	var result *C.struct__GtkWidget = nil
 
-	result := C.menu_item_new(C.int(id), ctitle, toBool(disabled), toBool(checked), toBool(separator))
+	if separator {
+		result = C.gtk_separator_menu_item_new()
+		C.gtk_widget_show(result)
+	} else {
+		ctitle := C.CString(title)
+		defer C.free(unsafe.Pointer(ctitle))
+
+		if checked {
+			result = C.gtk_check_menu_item_new_with_label(ctitle)
+
+			checkMenuItem := (*C.struct__GtkCheckMenuItem)(unsafe.Pointer(result))
+			C.gtk_check_menu_item_set_active(checkMenuItem, toBool(checked))
+		} else {
+			result = C.gtk_menu_item_new_with_label(ctitle)
+		}
+
+		C.gtk_widget_set_sensitive(result, toBool(!disabled))
+
+	    //
+	    // NOTE(nick): accelerators seem to require a window and an accel_group
+	    // Are they even supported in the AppIndicator?
+	    // As far as I can tell they don't ever show up in the AppIndicator menu...
+	    //
+	    // @see https://github.com/bstpierre/gtk-examples/blob/master/c/accel.c
+	    //
+	    /*
+	    GtkWindow *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	    GtkAccelGroup *accel_group = gtk_accel_group_new();
+	    gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
+
+	    gtk_widget_add_accelerator(item, "activate", accel_group, GDK_KEY_F7, 0, GTK_ACCEL_VISIBLE);
+	    */
+
+	    cactivate := C.CString("activate")
+	    defer C.free(unsafe.Pointer(cactivate))
+
+	    C._g_signal_connect(result, cactivate, C.go_menu_callback, C.int(id))
+
+	    C.gtk_widget_show(result)
+	}
+
+	//result := C.menu_item_new(C.int(id), ctitle, toBool(disabled), toBool(checked), toBool(separator))
+	//return toMenuItemHandle(result)
 	return toMenuItemHandle(result)
 }
 
 func MenuItemSetSubmenu(parent MenuItemHandle, child MenuHandle) {
-	C.menu_item_set_submenu(fromMenuItemHandle(parent), fromMenuHandle(child))
+	menuItem := (*C.struct__GtkMenuItem)(unsafe.Pointer(fromMenuItemHandle(parent)))
+	C.gtk_menu_item_set_submenu(menuItem, fromMenuHandle(child));
 }
 
 //export go_menu_callback
-func go_menu_callback(menuId int) {
+func go_menu_callback(item *C.struct__GtkMenuItem, menuId C.int) {
     if globalMenuCallback != nil {
-    	globalMenuCallback(menuId)
+    	globalMenuCallback(int(menuId))
     }
 }
 
@@ -84,12 +140,12 @@ func toBool(value bool) C.int {
 	return C.int(0)
 }
 
-func toMenuHandle(menu *C.struct__GtkMenuShell) MenuHandle {
+func toMenuHandle(menu *C.struct__GtkWidget) MenuHandle {
 	return (MenuHandle)(unsafe.Pointer(menu))
 }
 
-func fromMenuHandle(menu MenuHandle) *C.struct__GtkMenuShell {
-	return (*C.struct__GtkMenuShell)(unsafe.Pointer(menu))
+func fromMenuHandle(menu MenuHandle) *C.struct__GtkWidget {
+	return (*C.struct__GtkWidget)(unsafe.Pointer(menu))
 }
 
 func toMenuItemHandle(item *C.struct__GtkWidget) MenuItemHandle {
--- bridge/platform/linux/linux.h
+++ bridge/platform/linux/linux.h
@@ -1,23 +1,8 @@
 #include <gtk/gtk.h>
 #include <libappindicator/app-indicator.h>
 
-#define true 1
-#define false 0
+extern void go_menu_callback(GtkMenuItem *,int);
 
-typedef int bool;
-
-typedef void (*Menu_Callback)(int);
-typedef void (*Closure)();
-
-bool tray_init();
-AppIndicator *tray_indicator_new(char *id, char *png_icon_path, GtkMenuShell *menu);
-void tray_poll_events();
-
-GtkMenuShell *menu_new();
-void menu_append_menu_item(GtkMenuShell *menu, GtkWidget *item);
-void menu_set_callback(Menu_Callback  callback);
-
-GtkWidget *menu_item_new(int id, char *title, bool disabled, bool checked, bool separator);
-void menu_item_set_submenu(GtkWidget *parent, GtkMenuShell *child);
-
-void tray_test();
+static void _g_signal_connect(GtkWidget *item, char *action, void *callback, int user) {
+  g_signal_connect(item, action, G_CALLBACK(callback), (void *)user);
+}
