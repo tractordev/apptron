b1bf4a1d0c7c5e04e58f2de1dcf0c0c3775bbc54
Nick Aversano <nick23hi@gmail.com>
2023-04-05 20:48:03 -0400

    fix macos

--- bridge/api/app/app_darwin.go
+++ bridge/api/app/app_darwin.go
@@ -19,7 +19,7 @@ func init() {
 
 func SetMenu(m *menu.Menu) error {
 	app.SetMainMenu(m.NSMenu)
-	return menu.SetMenu(m)
+	return menu.SetMain(m)
 }
 
 func NewIndicator(icon []byte, items []menu.Item) {
@@ -55,10 +55,10 @@ func Run(options Options) error {
 		return !options.Agent
 	})
 	DelegateClass.AddMethod("applicationWillFinishLaunching:", func(notification objc.Object) {
-		mainMenu := menu.GetMenu()
+		mainMenu := menu.Main()
 		if mainMenu == nil {
-			menu.SetMenu(menu.New([]menu.Item{}))
-			mainMenu = menu.GetMenu()
+			menu.SetMain(menu.New([]menu.Item{}))
+			mainMenu = menu.Main()
 			app.SetMainMenu(mainMenu.NSMenu)
 		}
 		if options.Accessory {
